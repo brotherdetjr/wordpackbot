0911e594ea9c1e6ce54f7ac9133b14753b29f1f3
brotherdetjr <brotherdetjr@mail.ru>
2016-09-11 00:08:20 +0300

    Implemented test for 'not so fast' case. Made tests to run with different executors.

--- src/test/groovy/wordpackbot/StateControllerBaseTest.groovy
+++ src/test/groovy/wordpackbot/StateControllerBaseTest.groovy
@@ -2,16 +2,23 @@ package wordpackbot
 
 import groovy.util.logging.Log4j2
 import spock.lang.Specification
+import spock.lang.Unroll
 import spock.util.concurrent.BlockingVariable
 import wordpackbot.bots.ChatBot
 import wordpackbot.bots.UpdateEvent
 import wordpackbot.dummy.DummyController
 
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
 
 import static com.google.common.collect.Maps.newConcurrentMap
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor
+import static java.lang.Integer.parseInt
 import static java.util.concurrent.CompletableFuture.completedFuture
 import static java.util.concurrent.Executors.newFixedThreadPool
+import static java.util.concurrent.TimeUnit.SECONDS
+import static wordpackbot.StateControllerBase.NOT_SO_FAST_MESSAGE
+import static wordpackbot.VertxUtils.vertxExecutor
 
 @Log4j2
 class StateControllerBaseTest extends Specification {
@@ -20,17 +27,26 @@ class StateControllerBaseTest extends Specification {
             USER_1 = 2,
             USER_2 = 22,
             CHAT_1 = 3,
-            CHAT_2 = 30
+            CHAT_2 = 30,
 
+            EXECUTORS = [
+                    fixed1: newFixedThreadPool(1),
+                    fixed5: newFixedThreadPool(5),
+                    fixed20: newFixedThreadPool(20),
+                    vertx: vertxExecutor(),
+                    direct: directExecutor()
+            ]
+
+    @Unroll
     @SuppressWarnings("GroovyAccessibility")
-    def 'DummyController sends incremented state to a proper chat'() {
+    def 'DummyController sends incremented state to a proper chat. Executor: #executorName'() {
         given:
         def sender = Mock(Sender)
         def results = [
-                '2:33' : new BlockingVariable<Boolean>(),
-                '22:38': new BlockingVariable<Boolean>(),
-                '2:42' : new BlockingVariable<Boolean>(),
-                '22:40': new BlockingVariable<Boolean>()
+                '2:33' : blockingVar(),
+                '22:38': blockingVar(),
+                '2:42' : blockingVar(),
+                '22:40': blockingVar()
         ]
         def bot = new ChatBot() {
             @Override
@@ -41,7 +57,7 @@ class StateControllerBaseTest extends Specification {
                 completedFuture null
             }
         }
-        new DummyController(bot, newConcurrentMap(), newFixedThreadPool(5), 29).init()
+        new DummyController(bot, newConcurrentMap(), EXECUTORS[executorName], 29).init()
         when:
         bot.fire new UpdateEvent('4', USER_1, CHAT_1)
         results['2:33'].get()
@@ -62,9 +78,78 @@ class StateControllerBaseTest extends Specification {
         results['22:40'].get()
         then:
         1 * sender.send('22:40', CHAT_2)
+        where:
+        executorName << EXECUTORS.keySet()
+    }
+
+    @Unroll
+    @SuppressWarnings("GroovyAccessibility")
+    def '"not so fast" message is sent when previous request is not handled yet. Executor: #executorName'() {
+        given:
+        def serviceBlockingVar = blockingVar()
+        def blocking1 = blockingVar()
+        def blocking2 = blockingVar()
+        def service = new LongRunningService(newFixedThreadPool(1), serviceBlockingVar)
+        def sender = Mock(Sender)
+        def bot = new ChatBot() {
+            @Override
+            CompletableFuture<?> send(String text, Long chatId) {
+                log.debug "Sending '$text' to chat with id $chatId"
+                sender.send text, chatId
+                if (text == NOT_SO_FAST_MESSAGE) {
+                    blocking1.set true
+                } else if (text == '2:31') {
+                    blocking2.set true
+                }
+                completedFuture null
+            }
+        }
+        new DummyController(bot, newConcurrentMap(), EXECUTORS[executorName], 29) {
+            @Override
+            protected CompletableFuture<Integer> onUpdate(UpdateEvent event, Integer state) {
+                service.sum state, parseInt(event.text)
+            }
+        }.init()
+        when:
+        bot.fire new UpdateEvent('2', USER_1, CHAT_1)
+        bot.fire new UpdateEvent('3', USER_1, CHAT_1)
+        blocking1.get()
+        then:
+        1 * sender.send(NOT_SO_FAST_MESSAGE, CHAT_1)
+        when:
+        serviceBlockingVar.set true
+        blocking2.get()
+        then:
+        1 * sender.send('2:31', CHAT_1)
+        where:
+        executorName << EXECUTORS.keySet()
+    }
+
+    def BlockingVariable<Boolean> blockingVar() {
+        new BlockingVariable<Boolean>(5, SECONDS)
     }
 
     interface Sender {
         void send(String text, Long chatId)
+    }
+
+    class LongRunningService {
+
+        private final Executor executor
+        private final BlockingVariable<Boolean> monitor
+
+        LongRunningService(Executor executor, BlockingVariable<Boolean> monitor) {
+            this.executor = executor
+            this.monitor = monitor
+        }
+
+        CompletableFuture<Integer> sum(Integer a, Integer b) {
+            def future = new CompletableFuture<Integer>()
+            executor.execute {
+                monitor.get()
+                future.complete a + b
+            }
+            future
+        }
     }
 }
