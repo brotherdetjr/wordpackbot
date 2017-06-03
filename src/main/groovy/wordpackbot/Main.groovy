ed34988bf8f5f222cc95cd46f316dab393e133cb
brotherdetjr <brotherdetjr@mail.ru>
2017-06-03 22:52:31 +0300

    API update.

--- src/main/groovy/wordpackbot/Main.groovy
+++ src/main/groovy/wordpackbot/Main.groovy
@@ -1,13 +1,12 @@
 package wordpackbot
 
-import groovy.util.logging.Slf4j
 import brotherdetjr.pauline.telegram.TelegramEngine
+import groovy.util.logging.Slf4j
 import wordpackbot.dao.StubPlaybackSourceDao
 import wordpackbot.states.Playback
 import wordpackbot.states.StateFactory
 
 import static java.lang.Thread.currentThread
-import static brotherdetjr.pauline.telegram.TelegramUtils.extractUserId
 import static wordpackbot.VertxUtils.vertxExecutor
 
 @Slf4j
@@ -17,7 +16,7 @@ class Main {
 			.parse(currentThread().contextClassLoader.getResourceAsStream('config.groovy').text)
 		def stateFactory = new StateFactory(new StubPlaybackSourceDao(config))
 		TelegramEngine.builder(config.bot.token as String, config.bot.name as String)
-			.initial({ stateFactory.startPlayback extractUserId(it), 'тест' })
+			.initial({ stateFactory.startPlayback it.userId, 'тест' })
 			.handle().when(Playback).by({ event, playback -> ++playback })
 			.render(Playback).as({ it.renderer.send it.state.value })
 			.executor(vertxExecutor())
