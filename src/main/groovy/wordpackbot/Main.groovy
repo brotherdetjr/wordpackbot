24eae40595ce3b0a0756828ec7762c1ee75f8341
brotherdetjr <brotherdetjr@mail.ru>
2017-06-03 20:56:58 +0300

    Package renamings.

--- src/main/groovy/wordpackbot/Main.groovy
+++ src/main/groovy/wordpackbot/Main.groovy
@@ -1,13 +1,13 @@
 package wordpackbot
 
 import groovy.util.logging.Slf4j
-import puremvc.telegram.TelegramMvc
+import brotherdetjr.pauline.telegram.TelegramEngine
 import wordpackbot.dao.StubPlaybackSourceDao
 import wordpackbot.states.Playback
 import wordpackbot.states.StateFactory
 
 import static java.lang.Thread.currentThread
-import static puremvc.telegram.TelegramUtils.extractUserId
+import static brotherdetjr.pauline.telegram.TelegramUtils.extractUserId
 import static wordpackbot.VertxUtils.vertxExecutor
 
 @Slf4j
@@ -16,7 +16,7 @@ class Main {
 		def config = new ConfigSlurper()
 			.parse(currentThread().contextClassLoader.getResourceAsStream('config.groovy').text)
 		def stateFactory = new StateFactory(new StubPlaybackSourceDao(config))
-		TelegramMvc.builder(config.bot.token as String, config.bot.name as String)
+		TelegramEngine.builder(config.bot.token as String, config.bot.name as String)
 			.initial({ stateFactory.startPlayback extractUserId(it), 'тест' })
 			.handle().when(Playback).by({ event, playback -> ++playback })
 			.render(Playback).as({ it.renderer.send it.state.value })
